07ea5f9a0f26c3d10ba259210e83bc3035c437db
Khushi <k27kashyap@gmail.com>
2024-06-14 23:08:49 +0530

    third changes added

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -17,6 +17,7 @@ export default function GameBoard({ onActivePlayer, activePlayerSymbol }) {
             updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
             return updatedGameBoard;
         });
+
         onActivePlayer();
     }
     return (
