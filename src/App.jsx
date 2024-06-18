b894cb1c2abf634040bb984673f5951c90779eb2
Khushi <k27kashyap@gmail.com>
2024-06-18 20:19:30 +0530

    activePlayer error

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import Player from './Components/Player';
 import GameBoard from './Components/GameBoard';
-import { useState } from 'react';
+import { act, useState } from 'react';
 import Log from './Components/Log';
 
 function App() {
     const [gameTurns, setGameTurns] = useState([]);
     const [activePlayer, setActivePlayer] = useState('X');
 
-    function handleActivePlayer() {
+    function handleActivePlayer(rowIndex, colIndex) {
         setActivePlayer((currentPlayer) => (currentPlayer === 'X' ? 'O' : 'X'));
 
         setGameTurns((prevTurns) => {
             let currentPlayer = 'X';
 
-            if (prevTurns.lenght > 0 && prevTurns[0].player === 'X') {
+            if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
                 currentPlayer = 'O';
             }
 
             const updatedTurns = [
                 {
                     square: { row: rowIndex, col: colIndex },
-                    player: activePlayer,
+                    player: currentPlayer,
                 },
                 ...prevTurns,
             ];
